5c16404a16c3b58c71fa8cfcf5fee0c48bc390d7
hlyaaulia <hlyaaulia0910@gmail.com>
2024-12-03 21:18:57 +0700

    update

--- src/pages/api/komentar/index.ts
+++ src/pages/api/komentar/index.ts
@@ -18,24 +18,24 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
                     throw new Error('nama is required')
                 }
 
+                if( body.email == ""){
+                    throw new Error('email is required')
+                }
+
                 if( body.komentar == ""){
                     throw new Error('komentar is required')
                 }
 
-                if( body.content == ""){
-                    throw new Error('content is required')
-                }
-
-                let blogs = await db.collection("komentar").insertOne(body);
-                res.status(200).json({ data: blogs, message:'data berhasil di simpan' });
+                let komentar = await db.collection("komentar").insertOne(body);
+                res.status(200).json({ data: komentar, message:'data berhasil di simpan' });
 
             }catch(err){
                 res.status(422).json({ message: err.message});
             }
             break;
         default:
-            const blogsData = await db.collection("komentar").find({}).toArray();
-            res.json({ data: blogsData });
+            const blogsDataKomen = await db.collection("komentar").find({}).toArray();
+            res.json({ data: blogsDataKomen });
         break;
     }
 }
