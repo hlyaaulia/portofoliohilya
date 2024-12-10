ea430e12c54c33756b38e691ec5a54e25e67fbf3
hlyaaulia <hlyaaulia0910@gmail.com>
2024-12-10 20:16:02 +0700

    update register-login-dashboard

--- src/app/(globl)/login/page.js
+++ src/app/(globl)/login/page.js
@@ -90,11 +90,10 @@ export default function Login(){
                     </button>
                 </div>
             </form>
-
-            <p className="mt-10 text-center text-sm text-gray-500">
+  <p className="mt-10 text-center text-sm text-gray-500">
                 Not a member?{' '}
-                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
+                <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Register now
                 </a>
             </p>
             </div>
--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,7 +1,71 @@
+
+const ItemService=({
+  title,
+  subTitle
+})=>{
+
+  return (<>
+    <div className="w-[310px] bg-white p-5 ">
+      <div className="flex justify-center">
+        <img src="images/coding.png"/>
+      </div>
+      <div className="text-center">
+        <h3>{title}</h3>
+        <span className="text-[15px] text-[#767676]">{subTitle}</span>
+      </div>
+    </div>
+  </>)
+}
+
 export default function Dashboard() {
+  const myService = [
+    {title:'web development', subTitle:'blog, e-commerce'},
+    {title:'uI/uX design', subTitle:'Mobile app, website design'},
+    {title:'sound design', subTitle:'Voice Over, Beat Making'},
+    {title:'game design', subTitle:'Character Design, Props & Objects'},
+    {title:'photography', subTitle:'portrait, product photography'},
+    {title:'advertising', subTitle:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+  ]
+
     return (
       <>
-        Hallo hai
+        <div className='flex w-full bg-fuchsia-300 rounded-xl shadow-2xl '>
+          <div className="flex: 1 1 auto p-10">
+            <div className="font-extrabold text-[48px] mt-10">
+              <div>I&lsquo;m Karina AESPA </div>
+              <div>
+                <span className="text-[#FFB400]">leader</span> of aespa 
+              </div>
+            </div>
+            <p className="text-[#767676]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
+            <button className="btn-primary mt-5">
+              <span className="relative text-sm font-semibold text-white">
+                  Here Me
+              </span>
+            </button>
+          </div>
+          <div>
+            <img class="size-25.5" src="image/karina.jpg" />
+          </div>
+        </div>
+
+        <div className="mt-20 text-center">
+          <h2 className="text-[32px] font-bold">My services</h2>
+          <div className="flex justify-center text-[#767676]">
+            <p className="w-1/2	">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
+          </div>
+        </div>
+
+        <div className="grid grid-rows-2 grid-flow-col gap-3 mt-10">
+          {
+            myService.
+              map((item,key)=><ItemService
+                                key={key} 
+                                title={item.title} 
+                                subTitle={item.subTitle} 
+                     />)
+          }
+        </div>
       </>
     );
   }
