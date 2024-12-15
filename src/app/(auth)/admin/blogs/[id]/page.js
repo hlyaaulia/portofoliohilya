afe7ec2c175bc13853493b0a09e2324ec806219d
hlyaaulia <hlyaaulia0910@gmail.com>
2024-12-15 11:13:14 +0700

    updatess new uas 15.12.

--- src/app/(auth)/admin/blogs/[id]/page.js
+++ src/app/(auth)/admin/blogs/[id]/page.js
@@ -140,7 +140,7 @@ export default function EditBlogs() {
 
                 <Editor
                     id='content'
-                    apiKey='hz9os6h0p1826jcqknks4q1fm8yl9khctaa7nmexkf0rnx2e'
+                    apiKey='9cwimxs87anry0u2avnf1wswmlg849552261vhxbl2qb8qkw'
                     onInit={(_evt, editor) => editorRef.current = editor}
                     initialValue={data.content}
                     init={{
